f485f870d78d2bfc72b5a54d16f543f7a6eb9b20
A-Siddeeq <abubakaradamu1163@gmail.com>
2025-09-30 15:03:12 +0100

    fix API routes

--- task7_todo_list_ssr/pages/api/todos/[id].js
+++ task7_todo_list_ssr/pages/api/todos/[id].js
@@ -1,32 +1,14 @@
-import fs from "fs";
-import path from "path";
-
-const filePath = path.join(process.cwd(), "pages", "lib", "data", "todos.json");
-
-function readTodos() {
-  try {
-    return JSON.parse(fs.readFileSync(filePath, "utf8"));
-  } catch {
-    return [];
-  }
-}
-
-function writeTodos(todos) {
-  fs.writeFileSync(filePath, JSON.stringify(todos, null, 2));
-}
+let todos = [];
 
 export default function handler(req, res) {
   const { id } = req.query;
-  let todos = readTodos();
 
   if (req.method === "PATCH") {
     const { title } = req.body;
     todos = todos.map((todo) => (todo.id === id ? { ...todo, title } : todo));
-    writeTodos(todos);
     res.status(200).json({ success: true });
   } else if (req.method === "DELETE") {
     todos = todos.filter((todo) => todo.id !== id);
-    writeTodos(todos);
     res.status(200).json({ success: true });
   } else {
     res.status(405).end();
